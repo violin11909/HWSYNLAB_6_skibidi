569bcf214b639900e7ef5180fedc034dba38dade
violin11909 <sira2violin@gmail.com>
2025-03-17 23:14:51 +0700

    Update ImmGen.v
    Hang's update

--- src/ImmGen.v
+++ src/ImmGen.v
@@ -2,17 +2,25 @@ module ImmGen (
     input [31:0] inst,                  // instruction
     output reg signed [31:0] imm        // imm value (output)
 );
-    // ImmGen generate imm value base opcode
+    // ImmGen generates imm value base opcode
 
     wire [6:0] opcode = inst[6:0];
     always @(*) begin
         case(opcode)
             // TODO: implement your ImmGen here
             // Hint: follow the RV32I opcode map (table in spec) to set imm value
-            
-            
+            7'b0110111: imm = {inst[31:12], 12'b0};
+            7'b0010111: imm = {inst[31:12], 12'b0};
+            7'b1101111: imm = { {12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0 };
+            7'b1100111: imm = { {20{inst[31]}}, inst[31:20] };
+            7'b1100011: imm = { {19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0 };
+            7'b0000011: imm = { {20{inst[31]}}, inst[31:20] };
+            7'b0100011: imm = { {20{inst[31]}}, inst[31:25], inst[11:7] };
+            7'b0010011: imm = { {20{inst[31]}}, inst[31:20] };
+            default: imm = 32'd0;
+          
+
         endcase
     end
 
 endmodule
-
