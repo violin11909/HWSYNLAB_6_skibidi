f2ad6fb1bbca8a6000bdc37f49c04d2f95cd36c7
violin11909 <sira2violin@gmail.com>
2025-03-18 14:49:12 +0700

    Update Mux2to1.v

--- src/Mux2to1.v
+++ src/Mux2to1.v
@@ -2,21 +2,10 @@ module Mux2to1 #(
     parameter size = 32
 ) 
 (
-    input sel,                      // Selector
-    input signed [size-1:0] s0,     // Input 0
-    input signed [size-1:0] s1,     // Input 1
-    output signed [size-1:0] out    // Output
+    input sel,                     
+    input signed [size-1:0] s0,    
+    input signed [size-1:0] s1,    
+    output signed [size-1:0] out   // ✅ output เป็น wire ใช้ assign ได้เลย
 );
-    // TODO: implement your 2to1 multiplexer here
-    reg signed [size-1:0] Out;
-    assign out = Out;
-    always @(*) begin
-        case(sel)
-            1'b0: Out = s0;
-            1'b1: Out = s1;
-            default: Out = 0;
-        endcase
-    end
-    
+    assign out = (sel) ? s1 : s0;  // ✅ ใช้ assign ตรงๆ
 endmodule
-
