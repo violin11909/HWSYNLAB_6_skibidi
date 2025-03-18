a6ba0f51f1e41d86e8dd4aa6b43888dec656495d
violin11909 <sira2violin@gmail.com>
2025-03-18 14:48:17 +0700

    Update Control.v

--- src/Control.v
+++ src/Control.v
@@ -12,7 +12,86 @@ module Control (
 
     // TODO: implement your Control here
     // Hint: follow the Architecture (figure in spec) to set output signal
-    
+    always @(*) begin
+        //add it also have sub in maybe have to consider func7
+        //also have and but and func 3 = 111
+        //ADD 000/ SUB 000/ AND 111/ OR 110 / SLT 010
+        if (opcode == 7'b0110011) begin
+            memRead = 0;
+            memtoReg = 2'b00;
+            //ALUOP modify later
+            ALUOp = 3'b000;
+            memWrite = 0;
+            ALUSrc1 = 0;
+            ALUSrc2 = 0;
+            regWrite = 1;
+            PCSel = 0;
+        //ADDI 000// ANDI 111/ ORI 110 / SLTI 010
+        end else if (opcode == 7'b0010011) begin
+            memRead = 0;
+            memtoReg = 2'b00;
+            //ALUOP modify later
+            ALUOp = 3'b001;
+            memWrite = 0;
+            ALUSrc1 = 0;
+            ALUSrc2 = 1;
+            regWrite = 1;
+            PCSel = 0;
+        //LW 010
+        end else if (opcode == 7'b0000011) begin
+            memRead = 1;
+            memtoReg = 2'b01;
+            //ALUOP modify later
+            ALUOp = 3'b010;
+            memWrite = 0;
+            ALUSrc1 = 0;
+            ALUSrc2 = 1;
+            regWrite = 1;
+            PCSel = 0;
+        //store 010
+        end else if (opcode == 7'b0100011) begin
+            memRead = 0;
+            //dontcare memtoreg
+            //ALUOP modify later
+            ALUOp = 3'b011;
+            memWrite = 1;
+            ALUSrc1 = 0;
+            ALUSrc2 = 1;
+            regWrite = 0;
+            PCSel = 0;
+        //beq 000//bne 001//blt 100// bge 101
+        end else if (opcode == 7'b1100011) begin
+            memRead = 0;
+            //dc mtr
+            memWrite = 0;
+            ALUOp = 3'b100;
+            ALUSrc1 = 1;
+            ALUSrc2 = 1;
+            regWrite = 0;
+            PCSel = 1;
+        //jal
+        end else if (opcode == 7'b1101111) begin
+            memRead = 0;
+            memtoReg = 2'b10;
+            //ALUOP LATER
+            ALUOp = 3'b101;
+            memWrite = 0;
+            ALUSrc1 = 1;
+            ALUSrc2 = 1;
+            regWrite = 1;
+            PCSel = 1;
+        //jalr 000
+        end else if (opcode == 7'b1100111) begin
+            memRead = 0;
+            memtoReg = 2'b10;
+            //ALUOP LATER
+            ALUOp = 3'b110;
+            memWrite = 0;
+            ALUSrc1 = 0;
+            ALUSrc2 = 1;
+            regWrite = 1;
+            PCSel = 1;
+        end
+    end
 
 endmodule
-
