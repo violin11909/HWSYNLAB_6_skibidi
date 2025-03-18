c02ea30b92e1b3700c111ed3b2de6e41247b546f
violin11909 <sira2violin@gmail.com>
2025-03-18 14:48:05 +0700

    Update ALUCtrl.v

--- src/ALUCtrl.v
+++ src/ALUCtrl.v
@@ -11,8 +11,76 @@ module ALUCtrl (
     // See all R-type instructions in the lab and observe.
 
     // Hint: using ALUOp, funct7, funct3 to select exact operation
-    
+    //A + B  // ALUCTL == 0000
+    //A - B  // ALUCTL == 0001
+    //A && B // ALUCTL == 0010
+    //A || B // ALUCTL == 0011
+    //A < B  // ALUCTL == 0100
+    //A + B  but branch
+    //beq 0101
+    //bne 0110
+    //blt 0111
+    //bge 1000
+    always @(*) begin
+        if (ALUOp == 3'b000) begin
+            if (funct3 == 3'b000) begin
+                if (funct7) begin
+                    ALUCtl = 4'b0001;
+                end else begin
+                    ALUCtl = 4'b0000;
+                end
+            end else if (funct3 == 3'b111) begin
+                ALUCtl = 4'b0010;
+            end else if (funct3 == 3'b110) begin
+                ALUCtl = 4'b0011;
+            end else if (funct3 == 3'b010) begin
+                ALUCtl = 4'b0100;
+            end
+            
+            
+        end else if (ALUOp == 3'b001) begin
+            if (funct3 == 3'b000) begin
+                ALUCtl = 4'b0000;
+            end else if (funct3 == 3'b111) begin
+                ALUCtl = 4'b0010;
+            end else if (funct3 == 3'b110) begin
+                ALUCtl = 4'b0011;
+            end else if (funct3 == 3'b010) begin
+                ALUCtl = 4'b0100;
+            end
+            
+            
+        end else if (ALUOp == 3'b010) begin
+            //lw for sure
+            ALUCtl = 4'b0000;
+            
+            
+        end else if (ALUOp == 3'b011) begin
+            //sw for sure 
+            ALUCtl = 4'b0000;
+            
+            
+        end else if (ALUOp == 3'b100) begin
+            if (funct3 == 3'b000) begin
+                ALUCtl = 4'b0101;
+            end else if (funct3 == 3'b001) begin
+                ALUCtl = 4'b0110;
+            end else if (funct3 == 3'b100) begin
+                ALUCtl = 4'b0111;
+            end else if (funct3 == 3'b101) begin
+                ALUCtl = 4'b1000;
+            end
+            
+            
+        end else if (ALUOp == 3'b101) begin
+            ALUCtl = 4'b0000;
+        //jalr special case not only normal A+B
+        
+        
+        end else if (ALUOp == 3'b110) begin
+            ALUCtl = 4'b1001;
+        end
+    end
 
 
 endmodule
-
